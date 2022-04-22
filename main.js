4007403bc85842a273981e6198d425e32d620345
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-04-21 20:51:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,37 @@ song="";
 function preload(){
     song= loadSound("music.mp3");
 }
-
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0
 function setup(){
     canvas= createCanvas(600,500);
     canvas.center();
 
     video=createVideo(VIDEO);
     video.hide();
+
+    posenet= ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
+function modelLoaded(){
+    console.log("Model is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY);
+    }
+}
+
+
 
 function draw(){
     image(video,0,0,600,500);
@@ -18,4 +41,6 @@ function draw(){
 
 function playSound(){
     song.play();
+    song.rate(1);
+    song.setVolume(1);
 }
